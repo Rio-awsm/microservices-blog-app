30e1ddeb3c9a69deb64cb537df8a597a2224d2b3
Rio-awsm <mm.saha1982@gmail.com>
2025-05-27 23:45:25 +0530

    feat: implement update blog functionality with image upload support

--- services/author/src/controllers/author.contoller.ts
+++ services/author/src/controllers/author.contoller.ts
@@ -37,3 +37,59 @@ export const createBlog = TryCatch(async (req: AuthenticatedRequest, res) => {
     blog: result[0],
   });
 });
+
+export const updateBlog = TryCatch(async (req: AuthenticatedRequest, res) => {
+  const { id } = req.params;
+  const { title, description, blogcontent, category } = req.body;
+  const file = req.file;
+
+  const blog = await sql`SELECT * FROM blogs WHERE id = ${id}`;
+
+  if (!blog.length) {
+    res.status(404).json({
+      message: "No blog with this id",
+    });
+    return;
+  }
+
+  if (blog[0].author !== req.user?._id) {
+    res.status(401).json({
+      message: "You are not author of this blog",
+    });
+    return;
+  }
+
+  let imageUrl = blog[0].image;
+
+  if (file) {
+    const fileBuffer = getBuffer(file);
+
+    if (!fileBuffer || !fileBuffer.content) {
+      res.status(400).json({
+        message: "Failed to generate buffer",
+      });
+      return;
+    }
+    const cloud = await cloudinary.v2.uploader.upload(fileBuffer.content, {
+      folder: "blogs",
+    });
+
+    imageUrl = cloud.secure_url;
+  }
+
+  const updatedBlog = await sql`UPDATE blogs SET 
+  title = ${title || blog[0].title},
+  description = ${title || blog[0].description},
+  image= ${imageUrl},
+  blogcontent = ${title || blog[0].blogcontent},
+  category = ${title || blog[0].category}
+
+  WHERE id = ${id}
+  RETURNING *
+  `;
+
+  res.json({
+    message: "Blog Updated",
+    blog: updatedBlog[0],
+  });
+});
--- services/author/src/routes/author.routes.ts
+++ services/author/src/routes/author.routes.ts
@@ -1,10 +1,11 @@
 import express from "express"
-import { createBlog } from "../controllers/author.contoller.js"
+import { createBlog, updateBlog } from "../controllers/author.contoller.js"
 import { isAuth } from "../middlewares/isAuth.js"
 import uploadFile from "../middlewares/multer.js"
 
 const router = express()
 
 router.post("/blog/new", isAuth, uploadFile, createBlog)
+router.post("/blog/:id", isAuth, uploadFile, updateBlog)
 
 export default router
