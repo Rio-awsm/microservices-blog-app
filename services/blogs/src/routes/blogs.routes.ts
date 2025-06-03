187f75f139a8e0b4e024469e06aed38339a46bd4
Rio-awsm <mm.saha1982@gmail.com>
2025-06-04 04:28:33 +0530

    feat: implement comment functionality and authentication middleware for blog management

--- services/blogs/src/routes/blogs.routes.ts
+++ services/blogs/src/routes/blogs.routes.ts
@@ -1,8 +1,14 @@
 import express from "express"
-import { getAllBlogs, getSingleBlog } from "../controllers/blogs.controller.js"
+import { addComment, deleteComment, getAllBlogs, getAllComments, getSavedBlog, getSingleBlog, saveBlog } from "../controllers/blogs.controller.js"
+import { isAuth } from "../middleware/isAuth.js"
 
 const router = express.Router()
 router.get("/blog/all", getAllBlogs)
 router.get("/blog/:id", getSingleBlog)
+router.post("/comment/:id", isAuth, addComment);
+router.get("/comment/:id", getAllComments);
+router.delete("/comment/:commentid", isAuth, deleteComment);
+router.post("/save/:blogid", isAuth, saveBlog);
+router.get("/blog/saved/all", isAuth, getSavedBlog);
 
 export default router
